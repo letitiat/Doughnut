4661f3e5597135f9f7a1637bf168ad5342a29084
letitiat <46873537+letitiat@users.noreply.github.com>
2019-07-02 13:42:53 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,25 +1,69 @@
 var ctx = document.getElementById('doughnut1').getContext('2d');
+var ctx1 = document.getElementById('doughnut2').getContext('2d');
+var ctx2 = document.getElementById('doughnut3').getContext('2d');
 
 
 var doughnut1 = new Chart(ctx, {
   type: 'doughnut',
   data: {
     datasets: [{
-      data: [60, 40],
+      data: [90, 20],
       backgroundColor: [
-        'rgb(212, 150, 167, 1)',
-        'rgb(212, 150, 167, 0)'
+        'rgba(203, 243, 210, 1)', 'rgba(203, 243, 210, 0)'
          ],
-          borderWidth: 2
+				 borderColor: 'rgba(203, 243, 210, 0)',
 }],
+
 },
   options: {
+			events: [],
+			 cutoutPercentage: 90,
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+					display: false,
         }
   }
-};
+});
+
+var doughnut2 = new Chart(ctx1, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      data: [70, 30],
+      backgroundColor: [
+        'rgba(183, 192, 238, 1)', 'rgba(203, 243, 210, 0)'
+         ],
+				 borderColor: 'rgba(203, 243, 210, 0)',
+}],
+
+},
+  options: {
+			events: [],
+			 cutoutPercentage: 70,
+        scales: {
+					display: false,
+        }
+  }
+});
+
+var doughnut3 = new Chart(ctx2, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      data: [55,45],
+      backgroundColor: [
+				'rgba(247, 169, 168, 1)', 'rgba(203, 243, 210, 0)'
+         ],
+				 borderColor: 'rgba(203, 243, 210, 0)',
+
+}],
+
+},
+  options: {
+			events: [],
+			 cutoutPercentage: 50,
+        scales: {
+					display: false,
+        }
+  }
+});
+
